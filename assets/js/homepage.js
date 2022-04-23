5fec3d5017299ce5cba85284984440d861d74292
tevinalvord <tevin.alvord@gmail.com>
2022-04-23 17:00:56 -0600

    can search repos by popular languages with buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,22 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            })
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
 
 var displayRepos = function(repos, searchTerm) {
     // clear old content
@@ -50,9 +65,6 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
 
     }
-
-    console.log(repos);
-    console.log(searchTerm);
 };
 
 var getUserRepos = function(user) {
@@ -76,6 +88,17 @@ var getUserRepos = function(user) {
     })
 };
   
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
@@ -91,3 +114,4 @@ var formSubmitHandler = function(event) {
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
