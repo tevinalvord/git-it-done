22a6ccfe70db8c1bcbbe29b88933eddb94f17564
tevinalvord <tevin.alvord@gmail.com>
2022-04-23 14:52:20 -0600

    clicking single repo redirects to page that shows related issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // dispaly repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -58,6 +75,7 @@ var getRepoIssues = function(repo) {
     // create variable to hold the query
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -71,10 +89,10 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
-    console.log(repo);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
